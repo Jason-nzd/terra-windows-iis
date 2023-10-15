a829761706f5230dae2b5c22e5d60a414ce35266
Jason Burgess <jason.nzd@gmail.com>
2023-10-15 18:44:53 +1300

    removed unneeded outputs

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,6 @@
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.ec2_win
-}
-
 output "instance_http_address" {
   description = "Public http address of the EC2 instance"
   value       = "http://${aws_instance.ec2_win.public_dns}"
-}
-
-output "ssh_command" {
-  description = "SSH command for connecting to EC2 instance"
-  value       = "ssh -i ${var.ssh_key_local_path}${var.ssh_key_name}.pem ec2-user@${aws_instance.ec2_win.public_ip}"
 }
 
 output "rdp_user_name" {
@@ -18,12 +8,11 @@ output "rdp_user_name" {
   value       = "Administrator"
 }
 
-output "encrypted_rdp_password" {
-  description = "Encryped password for remote desktop, available 2-4 minutes after launch"
-  value       = aws_instance.ec2_win.password_data
-}
-
 output "decrypted_rdp_password" {
   description = "Decrypted password for remote desktop, available 2-4 minutes after launch"
   value       = rsadecrypt(aws_instance.ec2_win.password_data, file("${var.ssh_key_local_path}${var.ssh_key_name}.pem"))
+}
+
+output "remote_desktop_command" {
+  value = "mstsc /v:${aws_instance.ec2_win.public_ip} /prompt /w:1280 /h:1024"
 }
